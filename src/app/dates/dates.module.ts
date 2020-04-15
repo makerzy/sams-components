7e00d7ada4a93b17c9c80bbd4e42cb906a72b918
Samuel B <botassammzy@gmail.com>
2020-04-15 05:40:42 -0400

    merged components

--- src/app/dates/dates.module.ts
+++ src/app/dates/dates.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from "@angular/common";
 import { FormsModule } from "@angular/forms";
 
 import { IonicModule } from "@ionic/angular";
-
 import { DatesPageRoutingModule } from "./dates-routing.module";
 import { ComponentsModule } from "src/app/components/components.module";
 import { DatesPage } from "./dates.page";
